038d0a1ef6d114f3c6631814d565db7c8156d889
SatriyaDipa <oannonym47@gmail.com>
2019-09-28 03:12:52 +0700

    Menambahkan backstack dan Transition.

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -1,10 +1,14 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.BlueFragment;
+import polinema.ac.id.starterchapter05.fragments.RedFragment;
 
 public class DynamicActivity extends AppCompatActivity {
 
@@ -12,5 +16,34 @@ public class DynamicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic);
+    }
+
+    public void handleClickLoadRedFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder, new RedFragment(), "RED_TAG");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+//        getSupportFragmentManager().beginTransaction()
+//                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//                .replace(R.id.dynamic_fragment_placeholder, new RedFragment())
+//                .addToBackStack(null)
+//                .commit();
+
+    }
+
+    public void handleClickLoadBlueFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder, new BlueFragment(), "BLUE_TAG");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+//        getSupportFragmentManager().beginTransaction()
+//                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//                .replace(R.id.dynamic_fragment_placeholder, new BlueFragment())
+//                .addToBackStack(null)
+//                .commit();
     }
 }
