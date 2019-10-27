51e4f1da4f02c937eeee8767e10ab54d87256f1e
SatriyaDipa <oannonym47@gmail.com>
2019-10-27 09:57:36 +0700

    Tugas Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import polinema.ac.id.starterchapter05.activities.DynamicActivity;
 import polinema.ac.id.starterchapter05.activities.StaticActivity;
+import polinema.ac.id.starterchapter05.activities.TugasFragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleClickPraktikumFragment(View view) {
+        Intent intent = new Intent(this, TugasFragmentActivity.class);
+        startActivity(intent);
     }
 }
